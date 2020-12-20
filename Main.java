7e84d9dff57f53e2d6602c3a6bc493d30c9452be
Yoav Levi <yl@koala.one>
2020-12-20 23:34:21 +0000

    remove junk

--- Main.java
+++ Main.java
@@ -1,17 +1,13 @@
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.scene.control.*;
 import javafx.stage.*;
 import javafx.scene.*;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import jfxtras.styles.jmetro.JMetro;
 import jfxtras.styles.jmetro.Style;
 
 public class Main extends Application {
-    @FXML
-    private TableView<String> table;
     public static void main(String[] args) {
         launch(args);
     }
