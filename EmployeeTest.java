a201ebffd37932ba9960bc92094b2db01b80fe83
Yoav Levi <yl@koala.one>
2020-12-23 11:39:53 +0000

    employee testing

--- EmployeeTest.java
+++ EmployeeTest.java
@@ -6,13 +6,52 @@ public class EmployeeTest {
     Employee e = new Employee();
     
     @Test
-    public void setNameTest() {
-        String name = "Lmao";
-        assertEquals(name, e.setName(name).getName());
+    public void nameTest() {
+        String input = "Lmao";
+        assertEquals(input, e.setName(input).getName());
     }
+
+    @Test
+    public void surnameTest() {
+        String input = "Lmao";
+        assertEquals(input, e.setSurname(input).getSurname());
+    }
+
+    @Test
+    public void genderTest() {
+        String input = "male";
+        assertEquals(input, e.setGender(input).getGender());
+    }
+
+    @Test
+    public void birthdateTest() {
+        String input = "1990-10-10";
+        assertEquals(input, e.setBirthdate(input).getBirthdate());
+    }
+
+    @Test
+    public void ageTest() {
+        int input = 19;
+        assertEquals(input, e.setAge(input).getAge());
+    }
+
+    @Test
+    public void reqHolidayTest() {
+        String input = "20";
+        assertEquals(input, e.setReqHolidays(input).getReqHolidays());
+    }
+
     
     @Test
-    public void aSlowTest() {
-       System.out.println("Slow test");
+    public void remHolidayTest() {
+        String input = "20";
+        assertEquals(input, e.setRemHolidays(input).getRemHolidays());
+    }
+
+
+    @Test
+    public void calcAgeTest() {
+        String input = "1900-01-01";
+        assertEquals(120, e.calcAge(input));
     }
 }
